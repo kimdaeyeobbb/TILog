12472b8361f6f6c0962c4a7ea30fcee04000bd6e
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-07 17:07:47 +0900

    move: 경로 수정

--- 03/week2/src/day0307/hw/hw03/ConditionOperLab.java
+++ 03/week2/src/day0307/hw/hw03/ConditionOperLab.java
@@ -4,16 +4,18 @@ public class ConditionOperLab {
     public static void main(String[] args) {
         int randNum = (int)(Math.random()*5+1);
 //        System.out.println(randNum);
+        int result = 0;
         if (randNum == 1){
-            System.out.println(300+50);
+            result = (300+50);
         } else if (randNum == 2){
-            System.out.println(300-50);
+            result = (300-50);
         } else if (randNum == 3){
-            System.out.println(300*50);
+            result = (300*50);
         } else if (randNum == 4){
-            System.out.println(300/50);
+            result = (300/50);
         } else if (randNum == 5){  // else문으로 처리해도 됨 (애초에 1~5사이의 값이 나오므로)
-            System.out.println(300%50);
+            result = (300%50);
         }
+        System.out.println(result);
     }
 }
