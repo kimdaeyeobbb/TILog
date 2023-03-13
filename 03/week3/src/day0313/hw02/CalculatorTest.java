1f2ed6dcc519f5b5cd18aba526dfe55a124e8ece
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-14 08:48:03 +0900

    feat:  과제 구현

---
+++ 03/week3/src/day0313/hw02/CalculatorTest.java
@@ -0,0 +1,65 @@
+package day0313.hw02;
+
+import java.util.Scanner;
+
+class CalculatorExpr{
+    private int num1;
+    private int num2;
+
+    CalculatorExpr(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    int getAddition(){
+        return num1+num2;
+    }
+
+    int getSubtraction(){
+        if(num1>=num2){
+            return num1-num2;
+        } else {
+            return num2-num1;
+        }
+    }
+
+    int getMultiplication(){
+        return num1*num2;
+    }
+
+    double getDivision(){
+        double result;
+        if (num1 == 0){
+            result = (double) num1/num2;
+        } else if (num2 == 0){
+            result = (double) num2/num1;
+        } else {
+            result = (double) num1/num2;
+        }
+        return result;
+    }
+}
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+
+        int beingPlay = 1;
+        while (beingPlay != 0){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("입력된 숫자 : ");
+            int number1 = sc.nextInt();
+            int number2 = sc.nextInt();
+            if (beingPlay == 0){
+                break;
+            } else {
+                CalculatorExpr ce = new CalculatorExpr(number1,number2);
+                System.out.println("덧셈 : " +ce.getAddition());
+                System.out.println("뺄셈 : " +ce.getSubtraction());
+                System.out.println("곱셈 : " +ce.getMultiplication());
+                System.out.println("나눗셈 : " +ce.getDivision());
+            }
+            System.out.print("계속 하시곘습니까? (종료 하며려면 0입력): ");
+            beingPlay = sc.nextInt();
+        }
+    }
+}
---
+++ 03/week3/src/day0313/hw02/GradeTest.java
@@ -0,0 +1,64 @@
+package day0313.hw02;
+
+import java.util.Scanner;
+
+class GradeExpr{
+    private int jumsu[];
+
+    GradeExpr(int jumsu[]){
+        this.jumsu = jumsu;
+//        System.out.println("클래스 내부의 점수 : "+jumsu);
+    }
+
+    double getAverage(){
+        return (double) getTotal()/jumsu.length;
+    }
+
+    int getTotal(){
+        int sum = 0;
+        for (int i=0; i<jumsu.length; i++){
+            sum += jumsu[i];
+        }
+        return sum;
+    }
+
+    int getGoodScore(){
+        int good = 0;
+        for (int i=0; i< jumsu.length; i++){
+            if (good<jumsu[i]){
+                good = jumsu[i];
+            }
+        }
+        return good;
+    }
+
+    int getBadScore(){
+        int bad = 999;
+        for (int i=0; i< jumsu.length; i++){
+            if (bad>jumsu[i]){
+                bad = jumsu[i];
+            }
+        }
+        return bad;
+    }
+}
+
+public class GradeTest {
+    public static void main(String[] args) {
+        System.out.print("점수들 : ");
+        Scanner sc = new Scanner(System.in);
+        int score1, score2, score3, score4;
+
+        score1 = sc.nextInt();
+        score2 = sc.nextInt();
+        score3 = sc.nextInt();
+        score4 = sc.nextInt();
+
+        GradeExpr[] ge = new GradeExpr[4];
+        ge[0] = new GradeExpr(new int[]{score1,score2,score3,score4});
+        System.out.println("총점 : "+ge[0].getTotal());
+        System.out.println("평균 : "+ge[0].getAverage());
+        System.out.println("최고 점수 : " +ge[0].getGoodScore());
+        System.out.println("최저 점수 : " +ge[0].getBadScore());
+    }
+}
