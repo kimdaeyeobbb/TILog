c05abfd4b8a5bbeb29fae65130c7ad446cf91b6b
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-17 16:26:35 +0900

    style: 주석 추가

--- 03/week3/src/day0317/course/AccountTest.java
+++ 03/week3/src/day0317/course/AccountTest.java
@@ -1,30 +1,42 @@
 package day0317.course;
 class Account {
+	// 계좌와 관련된 객체에 대한 클래스
+
+	/* 멤버변수 */
 	private String account;
 	private double balance;
 	private double interestRate;
+	
+	/* 생성자 */
 	public Account() {
 	}
 	public Account(String account, double balance, double interestRate) {
 		setAccount(account);
 		setBalance(balance);
 		setInterestRate(interestRate);
-	}	
-	@Override  
+	}
+
+	/* 메서드 오버라이딩 */
+	// 얘를 지우면 ~
+	@Override
 	public String toString() {
 		return "계좌정보 : " + account + " " + balance + " " + interestRate;
 	}
+
+
 	public double calculateInterest() {
 		// 잔액기준 이자 계산		
 		return balance * interestRate*0.01;
 	}
-	public void deposit(double money) throws Exception {
+	public void deposit(double money) throws Exception {  // throws절 추가함
 		if (money > 0)
 			balance += money;
-		else
+		else  // throw 구문은 음의값일 경우 수행
 			throw new Exception("입금 금액이 0보다 적습니다.");
+		// Exception클래스에 대한 객체를 직접만들어서 에러 메시지 던짐
 	}
 	public void withdraw(double money) throws Exception {
+		// 현재 잔액보다 더 많은 금액을 인출하려고 할 때
 		if (money < 0 || balance < money)
 			throw new Exception("금액이 0보다 적거나 현재 잔액보다 많습니다.");
 		else
@@ -61,6 +73,7 @@ public class AccountTest {
 			System.out.println(e.getMessage());
 		}		
 		try {
+//			ac.deposit(-10);  // 얘처럼 구현하면 여기서 예외가 발생할 경우 바로 catch블록으로 넘어가므로 하단의 withdraw구문은 수행되지 않을 것임
 			ac.withdraw(600000);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
