983a9d620df7be14b2d2a2bfde835cfaa3634c0b
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-17 14:59:44 +0900

    chore: 예외처리 자료

--- 03/week3/src/day0317/course/ExceptionTest2.java
+++ 03/week3/src/day0317/course/ExceptionTest2.java
@@ -4,6 +4,9 @@ public class ExceptionTest2 {
 		System.out.println("수행시작");
 		try {
 			int num1 = Integer.parseInt(args[0]);
+			// 인자로  넘겨준것이 아무것도 없으므로
+			// 여기서 ArrayIndexOutOfBoundsException 발생
+
 			int num2 = Integer.parseInt(args[1]);
 			int result = num1/num2;
 			System.out.println("연산 결과 : "+result);
@@ -12,13 +15,15 @@ public class ExceptionTest2 {
 		} catch (ArithmeticException e) {
 		 System.out.println(e.getMessage());
 			System.out.println("두번째 프로그램 아규먼트는 0이 아닌 값을 전달하세요!!");
-			return;
+			return;   // 프로그램이 끝남. JVM에게로 돌아감. 어떤상황이든 finally를 수행하고 리턴함.
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 			System.out.println("프로그램 아규먼트로 숫자를 전달하세요!!");
-		}  finally {
+		}  catch(Exception e){
+			System.out.println("예외발생");
+		} finally {
 			System.out.println("항상 수행!!");
 		}
-		System.out.println("수행종료");
+		System.out.println("수행종료");  // return문 떄문에 얘는 실행되지 않을수도 있다
 	}
 }
--- 03/week3/src/day0317/course/ExceptionTest3.java
+++ 03/week3/src/day0317/course/ExceptionTest3.java
@@ -1,11 +1,16 @@
 package day0317.course;
 import java.util.Random;
+
 class TestException extends Exception {
+	// 멤버 - 생성자 하나 가지고 있음
+	// 얘는 객체 생성시 메시지 전달을 반드시 해야함 (최소한 null문자열처럼 비어있는 문자열이라도 전달해야 함)
 	TestException(String message){
 		super(message);
 	}
 }
+
 public class ExceptionTest3 {
+	// 콜스택에 main -> a -> b -> c 순서대로 올라감
 	public static void main(String[] args)  {
 		System.out.println("main()수행시작");
 		a();
@@ -17,6 +22,7 @@ public class ExceptionTest3 {
 			b();
 		} catch (TestException e){	
 			System.out.println("오류 발생 : "+e.getMessage());
+//			System.exit();  // 얘를 삽입하면 프로그램이 강제 종료됨 -> 웹을 다룰 때 얘를 쓰면 좋지 않음.
 		}
 		System.out.println("a()수행종료");
 	}
@@ -28,19 +34,16 @@ public class ExceptionTest3 {
 	static void c() throws TestException {
 		System.out.println("c()수행시작");
 		boolean flag = new Random().nextBoolean();
+		// 객체 생성하자마자 점 연산자 이용 (객체 생성해서 반드시 변수에 담아야한다는 생각은 안해도 됨)
+		// 객체가 가진 멤버를 2번 이상 사용하고 싶은 경우 변수에 담음
+		// nextBoolean을 2번이상 호출하고 싶으면 new Random()을 2번이상하는것은 적절하지 않으므로 변수를 생성해서 객체를 담아주자.
 		if(flag){
 			throw new TestException("<<:::::테스트로 예외발생시킴:::::>>");
-		}else {
+			// 여기서 예외 발생시키면 c메서드에서의 수행은 끝!
+			// 이 문장은 a메서드의 catch 블록이 내보냄
+		} else {
 			System.out.println("ㅋㅋㅋㅋ");
 		}	
 		System.out.println("c()수행종료");
 	}	
 }
-
-
-
-
-
-
-
-
