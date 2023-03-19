f3ce2dab4e18b1cd45f3127ceaad6284029d14fd
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-20 08:39:58 +0900

    style: 싱글톤 예제 코드 toString 부분 추가

--- 03/week3/src/day0316/course/MySingletonTest.java
+++ 03/week3/src/day0316/course/MySingletonTest.java
@@ -2,7 +2,6 @@ package day0316.course;
 
 class MySingleton {
 	 // 클래스가 메모리에 올라갈 때 (클래스가 로딩될 때)
-
 	// 메모리에 객체를 (미리) 생성하도록 만듦
 	private static MySingleton obj = new MySingleton();
 	private MySingleton() {		
@@ -32,6 +31,6 @@ public class MySingletonTest {
 		my.printFavoriteFood();
 		System.out.println(my);  // 전달된 객체의 toString 리턴값 (마이싱글톤이 toString이 없으므로 Object의 toString 호출)
 		System.out.println(MySingleton.getMy());  // 마이싱글톤 객체를 달라고 함
-		System.out.println(MySingleton.getMy());
+		System.out.println(my.toString());
 	}
 }
