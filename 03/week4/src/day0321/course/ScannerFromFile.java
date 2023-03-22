21d474ab150448896400fbc024e8c0f416faaf73
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-22 09:56:48 +0900

    chore: 0321 수업

---
+++ 03/week4/src/day0321/course/ScannerFromFile.java
@@ -0,0 +1,25 @@
+package day0321.course;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ScannerFromFile {
+	public static void main(String args[]) {
+		File f = new File("c:/iotest/output.txt"); // 파일 객체 생성
+		try (Scanner scan = new Scanner(f);) { // 스캐너 객체 생성
+			if(new Random().nextBoolean()) { // 난수 추출 (true/false)
+				System.out.println("[ 행 단위 ]");
+				while(scan.hasNext()) 
+					System.out.println(scan.nextLine());
+			} else {
+				System.out.println("[ 워드 단위 ]");
+				while(scan.hasNext()) 
+					System.out.println(scan.next());		
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일이 존재하지 않네요...ㅜ");
+		}					
+	}
+}
