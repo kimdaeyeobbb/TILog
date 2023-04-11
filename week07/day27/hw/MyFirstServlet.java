3ff2c79cbfae349f77599b2428c01408694a264b
DaeYeob Kim <kimdyk2@hanmail.net>
2023-04-11 16:46:27 +0900

    refactor: 과제 리팩토링 및 주석 추가

--- week07/day27/hw/MyFirstServlet.java
+++ week07/day27/hw/MyFirstServlet.java
@@ -13,6 +13,8 @@ import java.time.format.TextStyle;
 import java.util.Locale;
 
 @WebServlet("/myfirst")
+// localhost:8088/edu/myfirst?guestName=대니 => 이것이 쿼리문자열을 주는것임
+
 public class MyFirstServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
@@ -22,6 +24,9 @@ public class MyFirstServlet extends HttpServlet {
 
         /* 쿼리 문자열 추출 */
         String userName = request.getParameter("guestName");
+        // 서블릿 컨테이너가 HttpServletRequest request, HttpServletResponse response 객체를 생성
+        // getParameter ->
+
 
         /* 날짜 추출 */
         LocalDate currentDate = LocalDate.now();
@@ -34,7 +39,11 @@ public class MyFirstServlet extends HttpServlet {
         } else {
             out.print("<h2> GUEST님 반가워요.." + "오늘은 " + whatDay +"입니다!!" );
         }
+        out.close();   // close()를 안하면 톰캣은 알아서 close해주지만 다른 WAS는 그렇지 않으므로 close()를 빼면 안됨
     }
+
+    // 메서드를 따로 만들어도 됨
+//    private String getDay(){}
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         // post 기능 지원을 위해 doPost 오버라이딩
--- week07/day27/hw/VisitorServlet.java
+++ week07/day27/hw/VisitorServlet.java
@@ -12,12 +12,21 @@ import java.time.LocalDate;
 @WebServlet("/visitorpost")
 public class VisitorServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    // 객체 생성시점에 이미 시간이 정해져버리므로 static을 쓰는것은 좋지 않음
+//    static{
+//        date = LocaleDate.now()
+//    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // HttpServletRequest - 요청 관련 기능 (서블릿이 요청될떄까지 클라이언트로부터 요청된 정보를 추출하는 기능)
         // HttpServletResponse - 응답 관련 기능 (수행결과를 클라이언트에 응답할 때 응답용으로 사용함)
         response.setContentType("text/html; charset=utf-8");
         PrintWriter out = response.getWriter();
         // 문자기반 출력 스트림을 내보냄
+
+        /* post 방식일때 setCharacterEncoding 호출안하고 추출하면 한글이 깨짐 */
+        request.setCharacterEncoding("utf-8");
+        // 한글깨짐 방지
 
         String userName = request.getParameter("username");
         String opinion = request.getParameter("opinion");
@@ -25,18 +34,13 @@ public class VisitorServlet extends HttpServlet {
         /* 날짜 추출 */
         LocalDate today = LocalDate.now();
 
-
         out.print("<h2>"+ userName+"님이 "+ today.getYear() +"년 "+ today.getMonthValue()+"월 "+ today.getDayOfMonth()+"일에 남긴 글입니다.</h2>");
         out.print("<p>"+ opinion+"</p>");
 
-
         out.print("<a href='"+request.getHeader("referer")+"'>입력화면으로 가기</a>");
-        out.close();
-    }
+        // html에 되돌아가는 uri가 자동으로 만들어짐
+        // api를 이용하면 그때그때 요청한것에 대한 정보가 자동으로 생성됨. 따라서 uri가 효율적
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // post기능 지원하기 위해서 doPost 오버라이딩
-        request.setCharacterEncoding("utf-8");  // 한글 깨짐 방지
-        doGet(request, response);
+        out.close();
     }
 }
