7df4b0fd9077ef269b65b12bd69cc1eb49cc2544
DaeYeob Kim <kimdyk2@hanmail.net>
2023-04-26 12:27:50 +0900

    feat: 스프링실습7 과제 구현

---
+++ week09/day36/hw/StudyPathController.java
@@ -0,0 +1,26 @@
+package com.example.springedu.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class StudyPathController {
+    @RequestMapping(value = "/study/{num}/thymeleaf", produces = "text/html; charset=utf-8")
+    public RedirectView getRedirectURL(@PathVariable int num) {
+        String url="";
+        if(num == 1){
+            url = "https://abbo.tistory.com/56";
+        } else if (num == 2){
+            url = "https://abbo.tistory.com/57";
+        } else if (num == 3) {
+            url = "https://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html";
+        } else if (num == 4) {
+            url = "https://www.baeldung.com/dates-in-thymeleaf";
+        }
+        RedirectView redirectView =  new RedirectView();
+        redirectView.setUrl(url);
+        return redirectView;
+    }
+}
