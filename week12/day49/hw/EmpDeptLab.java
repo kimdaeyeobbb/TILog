85896e6b4d0a9c747d21e0a2d7736c78f4f35e92
Danny <kimdyk2@hanmail.net>
2023-05-16 17:09:51 +0900

    refactor: JPA 실습과제 리팩토링

--- week12/day49/hw/EmpDeptLab.java
+++ week12/day49/hw/EmpDeptLab.java
@@ -24,10 +24,10 @@ public class EmpDeptLab {
             jpql = "SELECT d.dname FROM Emp e JOIN e.dept d WHERE e.ename = :empname";
             TypedQuery<String> query = em.createQuery(jpql, String.class);
             query.setParameter("empname", empName);
-            try {
-                String deptName = query.getSingleResult();
-                System.out.printf("%s님의 부서명은 %s입니다.\n", empName, deptName);
-            } catch (Exception e) {
+            List<String> deptName = query.getResultList();
+            if(!deptName.isEmpty()) {
+                System.out.printf("%s님의 부서명은 %s입니다.\n", empName, deptName.get(0));
+            } else {
                 System.out.println("부서명을 찾을 수 없네요..ㅜㅜ");
             }
 
@@ -37,13 +37,14 @@ public class EmpDeptLab {
             jpql = "SELECT e.ename FROM Emp e JOIN e.dept d WHERE d.dname = :deptname";
             TypedQuery<String> query = em.createQuery(jpql, String.class);
             query.setParameter("deptname", deptName);
-            try {
-                List<String> empName = query.getResultList();
+            List<String> empName = query.getResultList();
+
+            if(!empName.isEmpty()) {
                 System.out.printf("%s 부서에 있는 직원들 이름입니다.\n", deptName);
                 for (String emp: empName) {
                     System.out.println(emp);
                 }
-            } catch (Exception e) {
+            } else{
                 System.out.println("직원을 찾을 수 없네요..ㅜㅜ");
             }
         }
@@ -52,4 +53,3 @@ public class EmpDeptLab {
         factory.close();
     }
 }
-
