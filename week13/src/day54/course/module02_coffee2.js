a10e2d778d72b7f7772546e5d4572bbaf265a3cf
Danny <kimdyk2@hanmail.net>
2023-05-22 14:40:09 +0900

    course: module

---
+++ week13/src/day54/course/module02_coffee2.js
@@ -0,0 +1,8 @@
+import unico from "./module02_coffee1.js";
+
+console.log("유니코: " + unico);
+
+console.log(unico.getName());
+
+unico.setName("Cappuccino");
+console.log(unico.getName());
---
+++ week13/src/day54/course/module03_coffee2.js
@@ -0,0 +1,7 @@
+import unico, { name } from "./module03_coffee1.js";
+
+console.log(name); // Americano
+console.log(unico.getName()); // Americano
+
+unico.setName("Espresso");
+console.log(unico.getName());
