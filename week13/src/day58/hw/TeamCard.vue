e31aa6706cde566f3edfd3ec2dbd494268509214
Danny <kimdyk2@hanmail.net>
2023-05-26 17:42:42 +0900

    feat: vue component 실습과제 구현

---
+++ week13/src/day58/hw/TeamCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card" style="width: 18rem; margin: auto">
+    <img
+        v-bind:src="srcUrl"
+        class="card-img-top"
+        width="70"
+        height="200"
+        alt="..."
+    />
+    <div class="card-body">
+      <h5 class="card-title">{{ name }}</h5>
+      <p class="card-text">{{ food }}</p>
+      <button class="btn btn-primary" @click="handleClick">OK</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+const p = defineProps({
+  name: String,
+  food: {
+    type: String,
+    default: "떡볶이",
+  },
+  srcUrl: String,
+  teamNum: Number,
+});
+
+const emit = defineEmits(["card-disappear"]);
+
+function handleClick() {
+  alert(`${p.teamNum}팀 입니다~~~!!`);
+  emit('card-disappear'); // 이벤트 발생
+}
+</script>
---
+++ week13/src/day58/hw/WorkView.vue
@@ -0,0 +1,63 @@
+<!-- WorkView.vue -->
+<template>
+  <div class="flex-container">
+    <TeamCard
+        team-num="5"
+        name="김대엽"
+        food="치킨"
+        src-url="/images/dooly.jpg"
+        v-if="showEl[0]"
+        @card-disappear="cardDisappear(0)"
+    />
+    <TeamCard
+        team-num="5"
+        name="김민성"
+        food="마파두부"
+        src-url="/images/minsung.png"
+        v-if="showEl[1]"
+        @card-disappear="cardDisappear(1)"
+    />
+    <TeamCard
+        team-num="5"
+        name="박주희"
+        food="1955버거"
+        src-url="/images/juhee.jpeg"
+        v-if="showEl[2]"
+        @card-disappear="cardDisappear(2)"
+    />
+    <TeamCard
+        team-num="5"
+        name="홍승희"
+        food="돈까스"
+        src-url="/images/seunghee.jpeg"
+        v-if="showEl[3]"
+        @card-disappear="cardDisappear(3)"
+    />
+  </div>
+</template>
+
+<script>
+import TeamCard from "@/components/TeamCard.vue";
+export default {
+  name: "WorkView",
+  components: {
+    TeamCard,
+  },
+  data() {
+    return {
+      showEl: [true, true, true, true], // 각 요소의 초기 상태 설정
+    };
+  },
+  methods: {
+    cardDisappear(index) {
+      this.showEl[index] = false;
+    },
+  },
+};
+</script>
+
+<style>
+.flex-container {
+  display: flex;
+}
+</style>
